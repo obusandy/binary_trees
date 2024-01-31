c3d9f4651b7494b5890c710142a7f1b2cdb1d045
obusandy <obungasandrah@gmail.com>
2024-01-31 08:16:42 -0800

    node binary

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,17 +2,16 @@
 
 /**
  * binary_tree_insert_right - a function that inserts a node aa
- * 							as the left-child of another node
+ * as the left-child of another node
  * @prnt: parent to the node to insert the right-child in.
  * @val: The value to store in the new node.
- *
  * Return: if the target parent node is absent, an error occurs
  * or NULL. Or else a pointer
  *
  * Description: whether parent already has a right-child, the new node
- *              takes its place and the old right-child is set as
- *              the right-child of the new node. thus maintaining the established
- * 				order of the tree.
+ * takes its place and the old right-child is set as
+ * the right-child of the new node. thus maintaining the established
+ * order of the tree.
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *prnt, int val)
 {
